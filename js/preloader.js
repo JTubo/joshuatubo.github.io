18b0c528897ff5d7a8059a5c856b376600816080
JTubo <joshuatubo4@gmail.com>
2024-11-12 23:46:50 +0800

    change CV directory and preloader time

--- js/preloader.js
+++ js/preloader.js
@@ -9,5 +9,5 @@ window.addEventListener("load", () => {
       document.querySelector(".main-wrapper").style.display = "block";
       document.body.style.overflow = "auto"; // Restore scrolling for full content view
     }, 500); // Matches the CSS transition duration (0.5s)
-  }, 5000); // Wait 5 seconds before starting the preloader fade out
+  }, 3000); // Wait 2 seconds before starting the preloader fade out
 });
